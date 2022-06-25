cafbdc2654c6d6df810b39340400d9481d69f8e1
ppost0 <p.post2321@yahoo.com>
2022-06-25 11:49:29 -0400

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let splitStr = str.split('');
+  let reverseStr = splitStr.reverse();
+  let result = reverseStr.join('')
+  return result;
 };
 
 // Do not edit below this line
