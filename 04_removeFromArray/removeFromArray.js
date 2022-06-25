11d91a7450d4afd8e102641402981fb4921fb2d1
ppost0 <p.post2321@yahoo.com>
2022-06-25 12:06:02 -0400

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  let removals = Array.prototype.slice.call(arguments, 1);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!(removals.includes(arr[i]))) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
