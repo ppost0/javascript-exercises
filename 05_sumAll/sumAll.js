e69b307d64eb82d7d41d59a80d6836699d5845d5
ppost0 <p.post2321@yahoo.com>
2022-06-25 13:53:02 -0400

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sum = 0;
+  let start;
+  let end;
+  if (x < 0 || y < 0) {
+    return 'ERROR';
+  }
+  if (typeof(x) !== 'number' || typeof(y) !== 'number') {
+    return 'ERROR';
+  }
+  if (x > y) {
+    start = y;
+    end = x;
+  } else {
+    start = x;
+    end = y;
+  }
+  while (start <= end) {
+    sum += start;
+    start++
+  }
+  return sum;
 };
 
 // Do not edit below this line
