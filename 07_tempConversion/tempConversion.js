701973d1de2b82fa513e55a74f3e669241f427cb
ppost0 <p.post2321@yahoo.com>
2022-06-25 14:09:21 -0400

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = temp - 32;
+  temp = temp * (5 / 9);
+  temp = Math.round(temp * 10) / 10
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * (9 / 5);
+  temp = temp + 32;
+  temp = Math.round(temp * 10) / 10
+  return temp;
 };
 
 // Do not edit below this line
