b608c291ec87aa83859c75ad313bb629f5440e6a
ppost0 <p.post2321@yahoo.com>
2022-07-13 12:11:07 -0400

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var numbers = arr;
+  let product = numbers.reduce(function(total, number) {
+    return total *= number;
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function(n, exp) {
+  return n ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 1) {
+    return 1;
+  }
+  if (n < 1) {
+    return 1;
+  }
+  if (n > 1) {
+    let factorial = 1;
+    for (let i = 1; i <= n; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
