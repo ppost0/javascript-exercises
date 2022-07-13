299dfd522905e423792d3d2f152dbc198972c650
ppost0 <p.post2321@yahoo.com>
2022-07-13 14:07:13 -0400

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let oldest;
+  let age = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i].yearOfDeath) {
+      arr[i].yearOfDeath = (new Date).getFullYear();
+    }
 
+
+    if (arr[i].yearOfDeath - arr[i].yearOfBirth > age) {
+      oldest = arr[i];
+      age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
