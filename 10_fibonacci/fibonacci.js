c4e6888669ec0c61bb67578b80dc35d4110ce581
ppost0 <p.post2321@yahoo.com>
2022-07-13 13:30:52 -0400

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  if (input < 0) {return 'OOPS'};
+  let a = 0;
+  let b = 1;
 
+  for (let i = 1; i < input; i++) {
+    let bOld = b;
+    b = a + b;
+    a = bOld;
+  }
+ return b;
 };
 
 // Do not edit below this line
