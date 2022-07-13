d737155700c51284f66187e9accb095cb9008479
ppost0 <p.post2321@yahoo.com>
2022-07-13 13:11:55 -0400

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 
+  function removePunctuation(string) {
+    return string.replace(regex, '');
+  }
+
+  input = removePunctuation(input); //remove punctuation
+  input = input.toLowerCase();  //make capital letters lowercase
+  input = input.replace(/\s+/g, ''); //remove space
+
+  let split = input.split('');
+  let reverse = split.reverse();
+  reverse = reverse.join('');
+
+  if (reverse === input) {
+    return true
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
